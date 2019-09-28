b80316b23f2c7d6230d068bf53255d3d56695309
hugodieb <hugodieb.hd@gmail.com>
2019-09-28 12:24:24 -0300

    automatizando comandinhos p docker

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,28 @@
-FROM node:alpine
+FROM python:3.6
+
+RUN apt-get update --fix-missing && \
+    apt-get install -y curl nano git locales zip unzip   
+
+# Replace shell with bash so we can source files
+RUN rm /bin/sh && ln -s /bin/bash /bin/sh && \
+    echo "export LS_OPTIONS='--color=auto'" >>~/.bashrc && \
+    echo "eval "\`dircolors\`"" >>~/.bashrc && \
+    echo "alias ls='ls \$LS_OPTIONS'" >>~/.bashrc && \
+    echo "alias ll='ls \$LS_OPTIONS -l'" >>~/.bashrc && \
+    echo "alias l='ls \$LS_OPTIONS -lA'" >>~/.bashrc
+
+RUN cd /tmp && \
+    wget --quiet https://nodejs.org/dist/v12.11.0/node-v12.11.0-linux-x64.tar.xz && \
+    tar xf node-v12.11.0-linux-x64.tar.xz && \
+    cp -r node-v12.11.0-linux-x64/* /usr && \
+    rm node-v12.11.0-linux-x64.tar.xz && \
+    npm install -g pm2
 
 WORKDIR /app
 
 COPY frontend/package.json frontend/package.json
-
 RUN cd frontend && npm install
-
 COPY frontend frontend
-
-COPY  docker/bin/* /usr/bin/
+RUN cd frontend && npm run build
+ENV SHELL=/bin/bash PYTHONUNBUFFERED=1
+COPY docker/bin/* /usr/bin/
---
+++ dev.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+RESTORE='\033[0m'
+RED='\033[00;31m'
+GREEN='\033[00;32m'
+YELLOW='\e[0;33m'
+HOST_PROD=3.130.167.180
+
+# Because nobody wants to be memorizing commands all the time
+# Instructions:
+# 1) ". dev.sh"
+# 2) "devhelp"
+# 3) Be happy
+
+
+# workon reactMock  # Change this to your project's name
+
+export PROJ_BASE="$(dirname "${BASH_SOURCE[0]}")"
+CD=$(pwd)
+cd $PROJ_BASE
+export PROJ_BASE=$(pwd)
+cd $CD
+
+#. ci/funcs.sh
+
+function devhelp {
+    echo -e "${GREEN}devhelp${RESTORE}           Prints this ${RED}help${RESTORE}"
+    echo -e ""       
+    echo -e "${GREEN}dkbuild${RESTORE}           Builds a ${RED}docker image${RESTORE} for this project"
+    echo -e ""
+    echo -e "${GREEN}dknpminstall${RESTORE}      Download node dependencies to ${RED}./node_modules/${RESTORE}"
+    echo -e ""    
+    echo -e "${GREEN}dkrun_dev${RESTORE}        Starts Frontend (dockerized) in dev mode."
+    echo -e ""    
+    echo -e "${GREEN}dk <command>${RESTORE}      Runs ${RED}<command>${RESTORE} inside main container"
+    echo -e "                  Example:"
+    echo -e "                   dk ${RED}bash${RESTORE}"
+    echo -e ""  
+    
+}
+
+function dkbuild {
+    CD=$(pwd)
+    cd $PROJ_BASE
+    docker build -t react .
+    exitcode=$?
+    cd $CD
+    return $exitcode
+}
+
+function dknpminstall {
+    CD=$(pwd)
+    cd $PROJ_BASE
+    docker run -it --rm -v $(pwd):/app -w /app/frontend -e NODE_ENV=development react npm install
+    exitcode=$?
+    cd $CD
+    return $exitcode
+}
+
+function dkrun_dev {
+    docker stop react
+    docker rm react
+    docker run --name react -it \
+        -p 3000:3000 \
+        react start_npm_dev.sh
+}
+
+function dk {
+    docker exec -it react $@
+}
+
+function echo_red {
+    echo -e "\e[31m$1\e[0m";
+}
+
+function echo_green {
+    echo -e "\e[32m$1\e[0m";
+}
+
+function echo_yellow {
+    echo -e "${YELLOW}$1${RESTORE}";
+}
+
+echo_green "Welcome to reactMock's dev env"
+echo_green "Hint: autocomplete works for the commands below ;)"
+echo_red   "------------------------------------------------------------------------"
+devhelp
