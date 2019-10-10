e2f069170f159e18e8bd7ca97386933bafa0287a
hugodieb <hugodieb.hd@gmail.com>
2019-10-10 11:54:29 -0300

    iniciando com redux

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Main from '@components/template/Main'
+import { connect } from 'react-redux';
 
-export default props =>
+const main =  props => 
     <Main icon="home" title="Início"
         subtitle="Seu projeto React com uso de dados mock">
         <div className='display-4'>Bem Vindo!</div>
         <hr />
         <p className="mb-0">Sistema de cadastro desenvolvido em React!</p>
+        <h1>{props.newValue}</h1>
     </Main>
+
+const mapStateToProps = store => ({
+    newValue: store.clickState.newValue
+  });
+
+export default connect(mapStateToProps)(main);
+// export default main
