e9c6232c577055241c7335d42e951ead67a66c32
hugodieb <hugodieb.hd@gmail.com>
2019-11-27 13:02:37 -0300

    mudanças no router

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,2 +1,20 @@
-import { connect } from 'react-redux'
-import AppApi from '~apijs'
+import AppApi from '~/apijs'
+
+class Auth {
+    constructor() {
+      this.authenticated = false
+    }
+    
+    authentication() {        
+        AppApi.whomi().then(response => {
+            this.authenticated = response.data.authenticated
+        })         
+    }
+  
+    isAuthenticated() {
+        return this.authenticated
+    }
+  }
+  
+  export default new Auth();
+  
