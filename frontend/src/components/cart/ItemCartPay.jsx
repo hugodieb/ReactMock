01531df4a7226960aeb18888a80fb5926096bebc
hugodieb <hugodieb.hd@gmail.com>
2020-04-16 12:42:21 -0300

    teste paypal com lib externa

--- frontend/src/components/cart/ItemCartPay.jsx
+++ frontend/src/components/cart/ItemCartPay.jsx
@@ -1,55 +1,99 @@
 import './ItemCartPay.css'
-import React from 'react'
+import React, {Component} from 'react'
+//import ReactDOM from 'react-dom'
 import paypal from '../../assets/imgs/paypal.png'
+import AppApi from '~apijs'
+import PaypalExpressBtn from 'react-paypal-express-checkout'
 
-const ItemCartPay = () => {
-    return (
-        <div className="item-cart-parent-checkout">
-            <div className="item-cart-child checkout">
-                <div className="columns">
-                    <div className="column">
-                        <h3>Resumo do pedido</h3>                       
-                    </div>                    
-                </div>
-                <div className="columns">
-                    <div className="column">
-                        <p>Template One</p>                      
+//let PayPalButton = window.paypal.Buttons.driver('react', { React, ReactDOM });
+
+class ItemCartPay extends Component {
+     
+    /*
+    componentWillMount() {
+        let pagarmeScript = document.createElement('script')
+        pagarmeScript.setAttribute('src', 'https://www.paypal.com/sdk/js?client-id=AZ-ZY7oH0r_xO-fdIK1DXqOvXkndkBb6daWTjT0dK5UiCNNYAZSrQQQ9Nq5XStccEZZ90zsAuXQ1dxmb')
+        document.head.appendChild(pagarmeScript)
+    }*/    
+
+    getToken = () => {
+       AppApi.getTokenPaypal().then(resp => {
+           //debugger
+           console.log("tokk", resp);
+           //this.pagamento(resp.data)
+       })
+    }
+    
+    pagamento(token) {
+        AppApi.paymentPaypal(token).then(resp => {
+            debugger
+            console.log('id pagamento', resp);
+        }) 
+    }
+
+    
+    render() {
+        const client = {
+            sandbox: "AZ-ZY7oH0r_xO-fdIK1DXqOvXkndkBb6daWTjT0dK5UiCNNYAZSrQQQ9Nq5XStccEZZ90zsAuXQ1dxmb",
+            production: "aa"
+          }                
+
+        return (
+            <div className="item-cart-parent-checkout">
+                <div className="item-cart-child checkout">
+                    <div className="columns">
+                        <div className="column">
+                            <h3>Resumo do pedido</h3>                       
+                        </div>                    
                     </div>
-                    <div className="column">
-                        <p>R$ 65.00</p>                      
-                    </div>                    
-                </div>
-                <hr/>
-                <div className="columns">
-                    <div className="column">
-                        <h3>Total</h3>                        
+                    <div className="columns">
+                        <div className="column">
+                            <p>Template One</p>                      
+                        </div>
+                        <div className="column">
+                            <p>R$ 65.00</p>                      
+                        </div>                    
                     </div>
-                    <div className="column">
-                        <p>R$ 65.00 <span>
-                            <strong>(1x no cartão)</strong>
-                        </span></p>                        
-                    </div>                                        
-                </div>
-                <div className="columns">
-                    <div className="column">
-                        <div className="item-imagem">                    
-                            <img className="item-img"
-                            src={paypal} />
+                    <hr/>
+                    <div className="columns">
+                        <div className="column">
+                            <h3>Total</h3>                        
+                        </div>
+                        <div className="column">
+                            <p>R$ 65.00 <span>
+                                <strong>(1x no cartão)</strong>
+                            </span></p>                        
+                        </div>                                        
+                    </div>
+                    <div className="columns">
+                        <div className="column is-4">
+                            <div className="item-imagem">                    
+                                <img className="item-img"
+                                src={paypal} />
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="pay">
+                                <PaypalExpressBtn client={client} currency={"BRL"} total={3231.50}
+                                    style= {
+                                        {'size': 'medium',
+                                        'color': 'blue',
+                                        'shape': 'rect',
+                                        'label': 'pay'}
+                                        }
+                                /> <button onClick={this.getToken}>pay</button>                                                                                       
+                            </div>
                         </div>
                     </div>
-                    <div className="column">
-                        <div className="pay">
-                            <a className="button is-large" >
-                                Pagamento
-                            </a>                            
-                        </div>
-                    </div>
-                </div>
-                <hr/>
-                <p>*Pagamento seguro com Paypal*</p>
+                    <hr/>
+                    <p>*Após clicar no botão acima,
+                        você será direcionado ao PayPal
+                        para realizar login e depois o pagamento.*
+                    </p>
+                </div>                
             </div>
-        </div>
-    )
+        )
+    }    
 }
 
 export default ItemCartPay
--- frontend/src/helpers/api/apimock.js
+++ frontend/src/helpers/api/apimock.js
@@ -7,7 +7,7 @@ function mockObject (data) {
     })
   }
 
-var loggedUser = null
+let loggedUser = null
 
 const api = {
     hello() {
@@ -64,17 +64,179 @@ const api = {
             authenticated: true,
             user: loggedUser
         } : {authenticated: false})
+    },
+
+    async getTokenPaypal() {        
+        //debugger
+        const PAYPAL_OAUTH_API = "https://api.sandbox.paypal.com/v1/oauth2/token"
+        const AUTH = {
+            username: 'AZ-ZY7oH0r_xO-fdIK1DXqOvXkndkBb6daWTjT0dK5UiCNNYAZSrQQQ9Nq5XStccEZZ90zsAuXQ1dxmb',
+            password: 'EG9LG36da1egjF0ETsGosheaoCx1Zja-VN2QrMGEywCFvBboN711DSRzPc265Y3Y0Bvn_Jsld-sQrV5t',
+          }
+          const HEADER = {
+            Accept: 'application/json',
+            'Accept-Language': 'en_US',
+            'content-type': 'application/x-www-form-urlencoded',
+          }
+          const PARAMS = {
+            grant_type: 'client_credentials'
+          }
+        try {
+            const { data: { access_token } } = await axios({
+              url: PAYPAL_OAUTH_API,
+              method: 'post',
+              headers: HEADER,
+              auth: AUTH,
+              params: PARAMS,
+            });
+        
+            console.log('access_token: ', access_token);
+            return mockObject(access_token)
+          } catch (error) {
+            console.log('error: ', error);
+        }        
+        
+    },
+
+    async getTokenPaypall() {        
+        //debugger
+        const PAYPAL_OAUTH_API = "https://api.sandbox.paypal.com/v1/oauth2/token"
+        const AUTH = {
+            username: 'AZ-ZY7oH0r_xO-fdIK1DXqOvXkndkBb6daWTjT0dK5UiCNNYAZSrQQQ9Nq5XStccEZZ90zsAuXQ1dxmb',
+            password: 'EG9LG36da1egjF0ETsGosheaoCx1Zja-VN2QrMGEywCFvBboN711DSRzPc265Y3Y0Bvn_Jsld-sQrV5t',
+          }
+          const HEADER = {
+            Accept: 'application/json',
+            'Accept-Language': 'en_US',
+            'content-type': 'application/x-www-form-urlencoded',
+          }
+          const PARAMS = {
+            grant_type: 'client_credentials'
+          }
+          const options = {              
+                headers: { 
+                    'Accept': 'application/json', 
+                    'Accept-Language': 'en_US',
+                    'Content-Type':'application/x-www-form-urlencoded'                     
+                },
+                params: { 'grant_type':'client_credentials' }
+          }
+        try {
+            //const { data: { access_token } } = await axios({
+            //  url: PAYPAL_OAUTH_API,
+            //  method: 'post',
+            //  headers: HEADER,
+            //  auth: AUTH,
+            //  params: PARAMS,
+            //});
+
+            const tok = await post(PAYPAL_OAUTH_API, AUTH, { params: options })
+        
+            console.log('access_token: ', tok);
+            return mockObject(tok)
+          } catch (error) {
+            console.log('error: ', error);
+        }        
+        
+    },
+
+    async paymentPaypal(token) {
+        //debugger
+        try {
+            const { data: { state } } = await axios({
+              url: 'https://api.sandbox.paypal.com/v1/payments/payment',
+              
+              headers: {                
+                'Accept-Language': 'en_US',
+                'content-type': 'application/json',
+                'Authorization': 'Bearer A21AAGnsavkooHM5yhbniw9EgdTk-tJ6oEASOpkUShJzo41Cyh9u6nXdA8OBapq16QaDkrTV4Rdh2YRiFUn_82V67XRx0QYaA'
+              },             
+              data: {
+                "intent": "sale",
+                "payer": {
+                    "payment_method": "paypal"
+                },
+                "transactions": [
+                    {
+                    "amount": {
+                        "total": "99.11",
+                        "currency": "USD",
+                        "details": {
+                        "subtotal": "99.00",
+                        "tax": "0.07",
+                        "shipping": "0.03",
+                        "handling_fee": "1.00",
+                        "shipping_discount": "-1.00",
+                        "insurance": "0.01"
+                        }
+                    },
+                    "description": "The payment transaction description.",
+                    "custom": "EBAY_EMS_90048630024435",
+                    "invoice_number": "48787589673",
+                    "payment_options": {
+                        "allowed_payment_method": "INSTANT_FUNDING_SOURCE"
+                    },
+                    "soft_descriptor": "ECHI5786786",
+                    "item_list": {
+                        "items": [
+                        {
+                            "name": "hat",
+                            "description": "Hot Dog.",
+                            "quantity": "5",
+                            "price": "3",
+                            "tax": "0.01",
+                            "sku": "1",
+                            "currency": "USD"
+                        },
+                        {
+                            "name": "handbag",
+                            "description": "Black handbag.",
+                            "quantity": "1",
+                            "price": "15",
+                            "tax": "0.02",
+                            "sku": "product34",
+                            "currency": "USD"
+                        }
+                        ],
+                        "shipping_address": {
+                        "recipient_name": "Brian Robinson",
+                        "line1": "4th Floor",
+                        "line2": "Unit #34",
+                        "city": "San Jose",
+                        "country_code": "US",
+                        "postal_code": "95131",
+                        "phone": "011862212345678",
+                        "state": "CA"
+                        }
+                    }
+                    }
+                ],
+                "note_to_payer": "Contact us for any questions on your order.",
+                "redirect_urls": {
+                    "return_url": "https://example.com/return",
+                    "cancel_url": "https://example.com/cancel"
+                }
+                
+              }
+            });
+            debugger
+            console.log('access_token: ', state);
+            return mockObject(state)
+          } catch (error) {
+            console.log('error: ', error);
+        }                
     }
 }
 
 export default api
 
-function get(url, params){    
-    return axios.get(url, params)
+function get(url, auth, params){    
+    return axios.get(url, auth, params)
 }
 
-function post(url, params){
-    return axios.post(url, params)
+function post(url, auth, params){
+    debugger
+    return axios.post(url, auth, params)
 }
 
 function put(url, params) {
