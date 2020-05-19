a4b06a3b2b745752797c20eeee38b87901e42cf7
hugodieb <hugodieb.hd@gmail.com>
2020-05-19 17:13:47 -0300

    separando as coisas

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from core import views
+from core import views, views_auth
 
 urlpatterns = [
-    path('api/whoami', views.whoami),
-    path('api/login', views.login),
-    path('api/logout', views.logout),
+    path('api/whoami', views_auth.whoami),
+    path('api/login', views_auth.login),
+    path('api/logout', views_auth.logout),
 
     #Templates
-    path('api/templates', views.getTemplates),
-    path('api/template', views.templateDetail),
+    path('api/templates', views.get_templates),
+    path('api/template', views.template_detail),
 ]
--- core/views.py
+++ core/views.py
@@ -1,35 +1,8 @@
-from django.http.response import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib import auth
+from django.http.response import JsonResponse
 from core.service import template_svc
-from core.models import User, Template
-
-AUTH_BACKEND = 'django.contrib.auth.backends.ModelBackend'
-
-@csrf_exempt
-def login(request):
-    email = request.POST['email']
-    password = request.POST['password']
-    user = User.objects.get(email=email)
-    user_dict = None
-    if user is not None:
-        if user.is_active and user.check_password(password):
-            auth.login(request, user, backend=AUTH_BACKEND)
-            user_dict = _user2dict(user)
-    return JsonResponse({'user': user_dict}, safe=False)
+from core.models import Template
 
 
-def logout(request):
-    auth.logout(request)
-    return HttpResponse('{}', content_type='application/json')
-
-
-def whoami(request):
-    i_am = {
-        'user': _user2dict(request.user),
-        'authenticated': True
-    } if request.user.is_authenticated else {'authenticated': False}
-    return JsonResponse(i_am)
 
 
 def get_templates(request):
@@ -48,8 +21,5 @@ def template_detail(request):
     return JsonResponse({'Template': detail}, safe=False)
 
 
-def _user2dict(user):
-    d = {}
-    d.update(user.profile.to_dict_json())
-    return d
+
 
