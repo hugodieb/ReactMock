45accae4f6e946ebb70fc1a7f4868d7e7ed71aa7
hugodieb <hugodieb.hd@gmail.com>
2020-05-21 21:01:00 -0300

    gera sku

--- core/commons/utils.py
+++ core/commons/utils.py
@@ -4,3 +4,8 @@ import random
 
 def random_invoice_number(size):
     return ''.join(random.choice(string.digits) for _ in range(size))
+
+
+def random_sku_template(size):
+
+    return ''.join(random.choices(string.ascii_uppercase + string.digits) for _ in range(size))
